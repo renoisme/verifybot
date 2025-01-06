d3ba08600a8b1bde85161ff20c974e1fe78b8c81
renoisme <renonosan221@gmail.com>
2025-01-06 16:14:53 +0900

    ますたーそーど

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Client, GatewayIntentBits, Partials, Events, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Events, ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageFlags } = require('discord.js');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -23,13 +23,13 @@ client.on(Events.ClientReady, () => {
 client.on(Events.MessageCreate, async (message) => {
     const { content, author, channel, guild } = message;
 
-    if (author.id !== '1186812822454276218') return;
-    if (content === '!verify') {
+    if (author.id === '1186812822454276218' && content === '!verify') {
         const ruleChannel = await guild.channels.fetch('1243957898455945408').catch(() => null);
         if (!ruleChannel || !ruleChannel.lastMessageId) {
             message.react('❌');
             return;
         }
+
         const ruleMessage = await ruleChannel.messages.fetch(ruleChannel.lastMessageId).catch(() => null);
         if (!ruleMessage || ruleMessage.content.length === 0) {
             message.react('❌');
@@ -58,29 +58,30 @@ client.on(Events.InteractionCreate, async (interaction) => {
         const role = await guild.roles.fetch('1243953263045513306').catch(() => null);
         if (!role) {
             interaction.reply({
-                content: 'ロールが見つかりませんでした',
-                ephemeral: true
+                content: 'ロールが見つかりませんでした，サーバー管理者にお問い合わせください',
+                flags: MessageFlags.Ephemeral
             });
             return;
         }
-        const isHasRole = member.roles.cache.has(role.id);
-        if (isHasRole) {
+
+        const hasRole = member.roles.cache.has(role.id);
+        if (hasRole) {
             interaction.reply({
                 content: 'すでにルールに同意しています',
-                ephemeral: true
+                flags: MessageFlags.Ephemeral
             });
         } else {
             try {
                 await member.roles.add(role);
                 interaction.reply({
                     content: 'ルールに同意しました',
-                    ephemeral: true
+                    flags: MessageFlags.Ephemeral
                 });
             } catch (err) {
                 console.error(err);
                 interaction.reply({
-                    content: 'エラーが発生しました',
-                    ephemeral: true
+                    content: 'エラーが発生しました，サーバー管理者にお問い合わせください',
+                    flags: MessageFlags.Ephemeral
                 });
             }
         }
